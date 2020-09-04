9a5d415506c9bef0088aedf49e8a729eac2a4efb
Freeler <freeleryt@gmail.com>
2020-09-04 03:22:26 +0300

    Added persistent storage, set player number

--- renderer.js
+++ renderer.js
@@ -1,13 +1,50 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process because
-// `nodeIntegration` is turned off. Use `preload.js` to
-// selectively enable features needed in the rendering
-// process.
 const $ = require('jquery');
+const path = require('path');
+const fs = require('fs');
+const Storage = require('./storage.js');
+const pop = require('popper.js');
+const boots = require('bootstrap');
 
-const a = 1;
 
-function addColumn() {
-    $(".players").html("lmao");
-}
+const storage = new Storage({
+    configName: 'game-state',
+    defaults: {
+        playerNumber: 0,
+        playerTable: [{
+                name: "",
+                unplayed: ["Kings", "Queens", "Diamonds", "Levate", "Total+", "Total-", "Whist", "Rentz"],
+                score: ["0"] // score.length should be  = 8 x (playerData.length)
+            },
+            {
+                name: "",
+                unplayed: ["Kings", "Queens", "Diamonds", "Levate", "Total+", "Total-", "Whist", "Rentz"],
+                score: ["0"]
+            },
+            {
+                name: "",
+                unplayed: ["Kings", "Queens", "Diamonds", "Levate", "Total+", "Total-", "Whist", "Rentz"],
+                score: ["0"]
+            }
+        ],
+        scoreTable: [ // 8 x 3
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0]
+        ]
+    }
+
+});
+
+
+
+$('#submitPlayerNumber').click(function() {
+
+    let n = $('#playerNumberSelector').val();
+    storage.set('playerNumber', n);
+    console.log("players:" + storage.get('playerNumber'));
+    $('#setupModal').modal('hide');
+});
+
+$(window).on('load', function() {
+    $('#setupModal').modal('show');
+});
